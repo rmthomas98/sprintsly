bdd0505e9e19ca9b870dd43e17f35cdb5935b2ac
Ryan <rmthomas1998@gmail.com>
2022-06-13 21:24:11 -0400

    pricing cards

--- components/PricingPlans/PricingPlans.tsx
+++ components/PricingPlans/PricingPlans.tsx
@@ -7,6 +7,7 @@ import {
   Checkbox,
   Row,
   Divider,
+  Spacer,
 } from "@nextui-org/react";
 import { BiCheck } from "react-icons/bi";
 
@@ -14,7 +15,15 @@ export const PricingPlans = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div className={styles.flexContainer}></div>
+        <div className={styles.flexContainer}>
+          <Card isHoverable css={{ padding: "$8" }}>
+            <Text size={14}>Free</Text>
+          </Card>
+          <Spacer />
+          <Card isHoverable css={{ padding: "$8" }}>
+            <Text>Free</Text>
+          </Card>
+        </div>
       </div>
     </div>
   );
